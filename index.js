52f1072246c1ea6dac113a79c01f28b4668dabec
chris <christopherkipp0@gmail.com>
2018-01-05 21:09:13 -0600

    storing both in sqlite and json now

--- index.js
+++ index.js
@@ -6,26 +6,28 @@ const program   = require('commander'),
         fs      = require('fs'),
         sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('gemynd.db');
 
+const db = new sqlite3.Database('gemynd.db');
 
 program
     .version(pkg.version)
     .command('log <entry> <time>')
     .description('Logs your entry')
-    .option('-l | --learn', 'signify learning')
+    .option('-l | --learn', 'signifies learning')
     .option('-a | --act', 'signifies action')
     .option('-r | --rest', 'signifies rest')
+    .option('-s | --social', 'signifies social')
     .option('-d | --date <date>', 'specifies the date if it was not today')
-    .option('-p | --place <place>', 'where thing was done')
+    .option('-p | --place <place>', 'location activity was done')
     .action(log);
 
 program.parse(process.argv);
 
-
 function log(entry, time, options){
-   
+
     let ea = []; //entryArray
+    let dbQuery = {};
+
     (options.date) ? ea[0] = options.date : ea[0] = new Date().toISOString().slice(0,10);
     if (options.learn) ea[1] = 'learn';
     if (options.act) ea[1] = 'act';
@@ -34,22 +36,45 @@ function log(entry, time, options){
     ea[3] = entry;
     (options.place) ? ea[4] = options.place : null;
 
-    console.log(ea);
+    dbQuery.createTable = "CREATE TABLE IF NOT EXISTS gemynd (date TEXT, category TEXT, time INT, notes TEXT, place TEXT)";
+    dbQuery.insertEntry = "INSERT INTO gemynd VALUES ('" + ea[0] + "', '" + ea[1] + "', " + ea[2] + ", '" + ea[3] + "', '" + ea[4] + "')";
 
-    db.serialize(() => {
-        db.run("CREATE TABLE IF NOT EXISTS gemynd (date TEXT, category TEXT, time INT, notes TEXT, place TEXT)");
-        
-          let stmt = db.prepare("INSERT INTO gemynd VALUES (?, ?, ?, ?, ?)");
-            stmt.run(ea[0], ea[1], Number(ea[2]), ea[3], ea[4])
-          
-          stmt.finalize();
-        
-          db.each("SELECT rowid AS id, date, category, notes, place FROM gemynd", (err, row) => {
-              console.log(row.id + " | " + row.date + " | " + row.category + " | " + row.time + " | " + row.notes + " | " + row.place);
-          });
+    db.serialize(() => {    
+        db.run(dbQuery.createTable);
+        db.run(dbQuery.insertEntry, (e) => {
+            if (e === null) {
+                jsonStore(dbQuery.insertEntry);
+            } else (
+                console.log(e)
+            )
         });
+
+          
+        db.each("SELECT rowid AS id, date, category, time, notes, place FROM gemynd", (err, row) => {
+            console.log(row.id + " | " + row.date + " | " + row.category + " | " + row.time + " | " + row.notes + " | " + row.place + '\n');
+        });
+    });
         
-        db.close();
+    db.close();
 }
 
- 
+function jsonStore (entry, js, ) {
+
+    let jlogs;
+    if (fs.existsSync('./gemynd.json')) {
+        jlogs = require('./gemynd.json');
+        let numEntries = Object.keys(jlogs).length;
+        jlogs[numEntries + 1] = entry
+        console.log('exists');
+    } else {
+        jlogs = {1 : entry};
+    }
+
+    fs.writeFile('gemynd.json', JSON.stringify(jlogs), 'utf8', (err) => {
+        if (err) throw err;
+        console.log('ándaga remembers');
+    });
+
+}
+
+
