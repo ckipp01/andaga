f7643ae02ea46b57e59bc771596badc1790db00a
ckipp01 <christopherkipp0@gmail.com>
2018-09-09 17:21:14 +0200

    some more tidying up

--- commands/tell.js
+++ commands/tell.js
@@ -1,23 +1,22 @@
-const sqlite3 = require('sqlite3').verbose(),
-      db = new sqlite3.Database('gemynd.db'),
-      where = require('./where');
-
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('gemynd.db')
+const where = require('./where')
 
 const getInfo = (options) => {
-  let tellQuery = 'SELECT SUM(time) FROM gemynd';
+  let tellQuery = 'SELECT SUM(time) FROM gemynd'
   where.form(tellQuery, options, (data) => {
     db.get(data, (err, row) => {
       if (err) {
-        console.log(' -|- ándaga error -|- \n\n' + err);
+        console.log(' -|- ándaga error -|- \n\n' + err)
       } else {
-        let total = row[Object.keys(row)[0]];
-        let whereQuery = data.replace(tellQuery, '');
-        console.log('\n -|- ándaga has logged ' + total + ' minutes of time' + whereQuery + ' -|-');
+        let total = row[Object.keys(row)[0]]
+        let whereQuery = data.replace(tellQuery, '')
+        console.log('\n -|- ándaga has logged ' + total + ' minutes of time' + whereQuery + ' -|-')
       }
-    });
+    })
 
-    db.close();
-  });
+    db.close()
+  })
 }
 
-module.exports.getInfo = getInfo;
+module.exports.getInfo = getInfo
--- commands/totals.js
+++ commands/totals.js
@@ -1,12 +1,10 @@
-const sqlite3 = require('sqlite3').verbose(),
-      db = new sqlite3.Database('gemynd.db'),
-      where = require('./where');
-
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('gemynd.db')
 
 const getTotals = (callback) => {
-  const getAllData = 'SELECT * FROM gemynd';
+  const getAllData = 'SELECT * FROM gemynd'
   executeAndSort(getAllData, (data) => {
-    callback(data);
+    callback(data)
   })
 }
 
@@ -16,7 +14,7 @@ const executeAndSort = (query, callback) => {
       total: 0,
       monthTotal: {}
     },
-    act:  {
+    act: {
       total: 0,
       monthTotal: {}
     },
@@ -35,95 +33,92 @@ const executeAndSort = (query, callback) => {
     total: 0
   }
   db.each(query, (err, row) => {
-    let monthName = getMonth(row.date);
+    let monthName = getMonth(row.date)
     switch (row.category) {
       case 'learn':
-        addMonthValues(totalsObject.learn.monthTotal, monthName, row.time);
-        totalsObject.learn.total = totalsObject.learn.total + row.time;
-        totalsObject.total = totalsObject.total + row.time;
-        break;
+        addMonthValues(totalsObject.learn.monthTotal, monthName, row.time)
+        totalsObject.learn.total = totalsObject.learn.total + row.time
+        totalsObject.total = totalsObject.total + row.time
+        break
       case 'act':
-        addMonthValues(totalsObject.act.monthTotal, monthName, row.time);
-        totalsObject.act.total = totalsObject.act.total + row.time;
-        totalsObject.total = totalsObject.total + row.time;
-        break;
+        addMonthValues(totalsObject.act.monthTotal, monthName, row.time)
+        totalsObject.act.total = totalsObject.act.total + row.time
+        totalsObject.total = totalsObject.total + row.time
+        break
       case 'rest':
-        addMonthValues(totalsObject.rest.monthTotal, monthName, row.time);
-        totalsObject.rest.total = totalsObject.rest.total + row.time;
-        totalsObject.total = totalsObject.total + row.time;
-        break;
+        addMonthValues(totalsObject.rest.monthTotal, monthName, row.time)
+        totalsObject.rest.total = totalsObject.rest.total + row.time
+        totalsObject.total = totalsObject.total + row.time
+        break
       case 'social':
-        addMonthValues(totalsObject.social.monthTotal, monthName, row.time);
-        totalsObject.social.total = totalsObject.social.total + row.time;
-        totalsObject.total = totalsObject.total + row.time;
-        break;
+        addMonthValues(totalsObject.social.monthTotal, monthName, row.time)
+        totalsObject.social.total = totalsObject.social.total + row.time
+        totalsObject.total = totalsObject.total + row.time
+        break
       default:
-        addMonthValues(totalsObject.unassigned.monthTotal, monthName, row.time);
-        totalsObject.unassigned.total = totalsObject.unassigned.total + row.time;
-        totalsObject.total = totalsObject.total + row.time;
-        break;
+        addMonthValues(totalsObject.unassigned.monthTotal, monthName, row.time)
+        totalsObject.unassigned.total = totalsObject.unassigned.total + row.time
+        totalsObject.total = totalsObject.total + row.time
+        break
     }
   }, () => {
     callback(totalsObject)
   })
-} 
+}
 
 const getMonth = (date) => {
-  let monthNumber = date.substring(date.indexOf('-') + 1, date.lastIndexOf('-'));
-  let monthName = '';
+  let monthNumber = date.substring(date.indexOf('-') + 1, date.lastIndexOf('-'))
+  let monthName = ''
   switch (monthNumber) {
     case '01':
-      monthName = 'January';
-      break;
+      monthName = 'January'
+      break
     case '02':
-      monthName = 'February';
-      break;
+      monthName = 'February'
+      break
     case '03':
-      monthName = 'March';
-      break;
+      monthName = 'March'
+      break
     case '04':
-      monthName = 'April';
-      break;
+      monthName = 'April'
+      break
     case '05':
-      monthName = 'May';
-      break;
+      monthName = 'May'
+      break
     case '06':
-      monthName = 'June';
-      break;
+      monthName = 'June'
+      break
     case '07':
-      monthName = 'July';
-      break;
+      monthName = 'July'
+      break
     case '08':
-      monthName = 'August';
-      break;
+      monthName = 'August'
+      break
     case '09':
-      monthName = 'September';
-      break;
+      monthName = 'September'
+      break
     case '10':
-      monthName = 'October';
-      break;
+      monthName = 'October'
+      break
     case '11':
-      monthName = 'November';
-      break;
+      monthName = 'November'
+      break
     case '12':
-      monthName = 'December';
-      break;
+      monthName = 'December'
+      break
     default:
-      monthName = 'Unassigned';
-      break;
+      monthName = 'Unassigned'
+      break
   }
-  return monthName;
+  return monthName
 }
 
 const addMonthValues = (obj, month, time) => {
   if (!(obj.hasOwnProperty(month))) {
-    obj[month] = time;
+    obj[month] = time
   } else {
-    obj[month] += time;
+    obj[month] += time
   }
 }
 
-
-
-
-module.exports.getTotals = getTotals;
+module.exports.getTotals = getTotals
