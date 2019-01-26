211dfca1354b2eab20d1da04a4f3ad2abbd71f75
ckipp01 <christopherkipp0@gmail.com>
2019-01-26 08:52:30 +0100

    starting to put in recall

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 'use strict'
 
-const program = require('commander')
-const pkg = require('./package.json')
-const log = require('./lib/commands/log.js')
 const helper = require('./lib/utils/helpers.js')
+const log = require('./lib/commands/log').log
+const program = require('commander')
+const recall = require('./lib/commands/recall').recall
 
 program
-  .version(pkg.version)
   .command('log <type> <entry> <time>')
   .description('logs your entries')
   .option('-d | --date <date>', 'specifies the date if it was not today')
   .option('-l | --location <location>', 'specifies location activity was done')
   .option('-p | --project <project>', 'specifies the related project')
   .option('-t | --tag <tag>', 'specifies related projects or tasks', helper.list)
-  .action(log.log)
+  .action(log)
+
+program
+  .command('recall')
+  .description('recalls your latest entries')
+  .option('-d | --date <date>', 'recalls entries on a specific date')
+  .option('-l | --location <location>', 'recalls entries on a certain day')
+  .option('-p | --project <project>', 'recalls entries on a certain project')
+  .option('-t | --tag <tag>', 'recalls entries related to certain tags')
+  .action(recall)
 
 program.parse(process.argv)
 
 if (program.args.length === 0) {
-  console.log('\n -|-|-|-|-|-  log and ándaga will remember  -|-|-|-|-|-\n |-|-|-|-|-  ask me to list and I will show  -|-|-|-|-|\n -|-|-|-  ask me to tell and I will do the math  |-|-|-\n |-|-|-|-  ask for help and it will be given  |-|-|-|-| ')
+  console.log('\n ándaga')
 }
