95b4a57c01c6c774039530184df1da313a2d092e
ckipp01 <ckipp@pm.me>
2019-02-04 20:08:04 +0100

    small change on tag

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ program
   .option('-d | --date <date>', 'specifies the date if it was not today')
   .option('-l | --location <location>', 'specifies location activity was done')
   .option('-p | --project <project>', 'specifies the related project')
-  .option('-t | --tag <tag>', 'specifies related projects or tasks', helper.list)
+  .option('-t | --tags <tags>', 'specifies related projects or tasks', helper.list)
   .action(log)
 
 program
@@ -20,7 +20,7 @@ program
   .option('-d | --date <date>', 'recalls entries on a specific date')
   .option('-l | --location <location>', 'recalls entries on a certain day')
   .option('-p | --project <project>', 'recalls entries on a certain project')
-  .option('-t | --tag <tag>', 'recalls entries related to certain tags')
+  .option('-ts | --tags <tag>', 'recalls entries related to certain tags')
   .action(recall)
 
 program.parse(process.argv)
